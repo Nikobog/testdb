fc0feb8f577bd86b1e6d002c0a980762c23bd712
sergeibogdaniuk <sergei.bogdaniuk@gmail.com>
2022-08-05 16:03:39 +0300

    feat(services): refactor services

--- src/services/http.js
+++ src/services/http.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-export const $http = axios.create({
+export const http = axios.create({
     baseURL: 'http://localhost:3001'
 })
---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+export * as api from './api'
+
+export { http } from './http'
